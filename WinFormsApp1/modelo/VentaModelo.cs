e14130bcc474462b904d50d0cbe96c1058496e0f
apaza3000 <96398557+apaza3000@users.noreply.github.com>
2024-10-25 20:29:08 -0500

    modelo create

--- WinFormsApp1/modelo/VentaModelo.cs
+++ WinFormsApp1/modelo/VentaModelo.cs
@@ -4,61 +4,94 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TpdNoche.entidad;
 
 namespace TpdNoche.modelo
 {
     public class VentaModelo
     {
         Conexion conexion;
+
+        public string MsgError = "";
         public VentaModelo() { 
             conexion = new Conexion();
         
         }
 
-        public void Create()
+        public int Create(EVenta data)
         {
-            var conn = conexion.getConexion;
-
-            conn.Open();
-
-            MySqlTransaction mst = conn.BeginTransaction();
-
-            MySqlCommand cmd = conn.CreateCommand();
-            try
-            {
-
-         
-           
-
-           //INSERTAR VENTA
+            var con = conexion.getConexion;
+            MySqlTransaction mst = con.BeginTransaction();
+            MySqlCommand cmd = con.CreateCommand();
             cmd.Transaction = mst;
 
-            cmd.CommandText = "";// sql para primera operacion
+            string tabla_venta = "venta";
+            string tabla_dventa = "detalle_venta";
+            try
+            {                
+                //empieza la primera operacion
+                //insertar venta
+                cmd.CommandText = $"INSERT INTO {tabla_venta} " +
+                    $"(fecha_venta,cliente_id,total,tipo_pago" +
+                    $",metodo_pago,observaciones) VALUES (" +
+                    $"@fech,@cli,@tot,@tp,@mp,@obs)";
 
-            cmd.Parameters.AddWithValue("@nombre", "Producto 1");
-      
-            cmd.ExecuteNonQuery();
+                cmd.Parameters.AddWithValue("@fech", data.Fecha_venta.ToString("yyyy-MM-dd"));
+                cmd.Parameters.AddWithValue("@cli", data.Cliente_id);
+                cmd.Parameters.AddWithValue("@tot", data.Total);
+                cmd.Parameters.AddWithValue("@tp", data.Tipo_pago);
+                cmd.Parameters.AddWithValue("@mp", data.Metodo_pago);
+                cmd.Parameters.AddWithValue("@obs", data.Observaciones);
 
-            //INSERTAR DETALLE DE VENTA
-            // Segunda operación 
-            cmd.CommandText = "UPDATE product SET stock = stock - 10 WHERE id = @id";
-            cmd.Parameters.Clear();
-            cmd.Parameters.AddWithValue("@id", 1);
-            cmd.ExecuteNonQuery();
+                con.Open();
 
-                //INSERTAR EL REGISTRO
-            mst.Commit();
+                int res=cmd.ExecuteNonQuery();
+                //termina la primera operacion
+                if (res > 0 )
+                {
+                    //comienza segunda operacion
+                    cmd.Parameters.Clear();
 
-            }catch (Exception ex)
-            {
+                    cmd.CommandText = "SELECT LAST_INSERT_ID()";
+                    int id_venta=Convert.ToInt32(cmd.ExecuteScalar());
+                    if (id_venta > 0)
+                    {
+                        int contador = 0;
+                        foreach (var item in data.Detalles)
+                        {
+                            cmd.Parameters.Clear();
+                            //EJECUCION DE LAS OPERACIONES DE DETALLE VENTA
 
+                            cmd.CommandText = $"INSERT INTO {tabla_dventa} (" +
+                                $"venta_id,producto_id,cantidad,precio) " +
+                                $"VALUES (@vi,@pi,@c,@p) ";
+                            cmd.Parameters.AddWithValue("@vi",id_venta);
+                            cmd.Parameters.AddWithValue("@pi", item.Producto_id);
+                            cmd.Parameters.AddWithValue("@c", item.Cantidad);
+                            cmd.Parameters.AddWithValue("@p", item.Precio);
+                            contador += cmd.ExecuteNonQuery();
+                        }
+
+                        if (contador == data.Detalles.Count)
+                        {
+                            mst.Commit();
+                            return 1;
+                        }
+                    }
+                }
                 mst.Rollback();
-
-
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                mst.Rollback();
+                MsgError=ex.Message;
+                return -1;
             }
 
-
-
+            //return 1===exitoso 
+            //return -1======entro cacht //mensaje de eror
+            //return 0====hubo algun errror no hay mensaje
 
         }
     }
